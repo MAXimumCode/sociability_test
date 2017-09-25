38ad3f6b087242ce02b02df0819d1d2ddecf9a44
MaksPro <mkozaev@mail.ru>
2017-09-24 20:03:15 -0700

    v2.0

--- main.rb
+++ main.rb
@@ -7,18 +7,27 @@ if Gem.win_platform? && ARGV[0]
   name = name.encode("UTF-8")
 end
 
+file_path = "#{File.dirname(__FILE__)}/data/"
+
+begin
+  questions = File.new("#{file_path}questions.txt").readlines
+  results = File.new("#{file_path}results.txt").readlines
+rescue SystemCallError
+  abort "Файлы вопросов и ответов не найдены."
+end
+
 if name == nil
   name = "Анонимус"
 end
 
 puts "Добрый день, #{name}! Вашему вниманию предлагается несколько простых вопросов. Отвечайте быстро, однозначно: «да», «нет», «иногда»."
 
-test = Test.new
+test = Test.new(questions)
 
 until test.finished?
   test.next_question
   test.count
 end
 
-result_printer = Result.new
+result_printer = Result.new(results)
 result_printer.result_test(test)
